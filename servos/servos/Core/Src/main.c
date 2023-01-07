6c75ebae4ea5da882da4a92fb2ae93c46195dd56
manuu22 <m.cvega@alumnos.upm.es>
2023-01-07 13:58:06 +0100

    FINAL

--- servos/servos/Core/Src/main.c
+++ servos/servos/Core/Src/main.c
@@ -71,7 +71,7 @@ volatile int16_t señal=0;
 volatile int16_t señal2=0;
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){//detectamos presencia
-	if (GPIO_Pin==GPIO_PIN_0)
+	if (GPIO_Pin==GPIO_PIN_8)
 	{
 		señal=0;
 		 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0);
@@ -82,7 +82,9 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){//detectamos presencia
 
 
 	}
+
 }
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)//acaba el temporizador
 {
   /* Prevent unused argument(s) compilation warning */
@@ -97,6 +99,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)//acaba el temporizad
 	 	HAL_TIM_Base_Stop_IT(&htim2);
 	 	 HAL_TIM_Base_Start_IT(&htim3);
 	 	señal=1;
+	 	HAL_TIM_Base_Stop(&htim2);
 
   }
   if(htim->Instance==TIM3)
@@ -201,6 +204,10 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1)==1)
+	  {
+		  HAL_TIM_Base_Stop_IT(&htim2);
+	  }
 
 	  if(señal==0)
 	  	 {
@@ -356,9 +363,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 65000;
+  htim2.Init.Prescaler = 50000;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 50000;
+  htim2.Init.Period = 30000;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -401,9 +408,9 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 30000;
+  htim3.Init.Prescaler = 32000;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 6000;
+  htim3.Init.Period = 2000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -505,9 +512,9 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13|GPIO_PIN_14|LD6_Pin|GPIO_PIN_4, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PA0 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  /*Configure GPIO pin : PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
@@ -518,9 +525,15 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_IRQn, 1, 0);
-  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
 
